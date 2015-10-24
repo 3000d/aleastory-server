51b3a4b212baaba6ef27af6df1ed86ca4cfceed1
Jonathan Berger <john@3kd.be>
2015-10-24 17:18:37 +0200

    debug

--- client.js
+++ client.js
@@ -4,6 +4,7 @@
   $('#send-text__form').on('submit', function(e) {
     e.preventDefault();
 
+    console.log('sending message through socket');
     socket.emit('send-text', $('#send-text__input').val());
   });
 }(jQuery));
